d441bfff56ffe01c62d2ce7776fa237209033535
greengypsy <ckumar073@gmail.com>
2022-05-26 20:40:59 +0930

    trying out different style sas

--- conf.py
+++ conf.py
@@ -76,9 +76,12 @@ release = '2.8.0'
 html_theme = 'furo'
 html_logo = 'assets/OpenMS_transparent_background.png'
 
-pygments_style = 'xcode'
+#pygments_style = 'xcode'
 
 pygments_dark_style = 'rrt'
+
+pygments_style = 'sas'
+
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
