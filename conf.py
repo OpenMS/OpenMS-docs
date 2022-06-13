a1a7f695e9e3d1908972cad24f9ab478c87538c6
Tapasweni Pathak <tapaswenipathak@gmail.com>
2022-06-13 08:23:29 +0530

    Revert "[FIX] allow crosslinking in markdown"
    This reverts commit 0241aa2c1c9e4ffd754ab0a6dea7ffd98967c15a.

--- conf.py
+++ conf.py
@@ -46,9 +46,6 @@ myst_enable_extensions = [
   "linkify_fuzzy_links",
 ]
 
-# Generate header anchors for cross-linking in markdown until depth n
-myst_heading_anchors = 3
-
 autosummary_generate = True
 autosummary_imported_members = True
 
