81957341922d5831685d3a71a25d32c6c0c99df2
Ketan Saini <124780734+ketan1406@users.noreply.github.com>
2025-03-12 20:45:29 +0530

    Incorporation of new logo for OpenMS docs (#254)
    * Added new logo whiteFont
    
    * Update references to new logo & favicon
    
    * Replaced old logo and favicon
    
    * Update logo config for light/dark mode
    
    * Update removed html_static_path redundancy
    
    * Update static_path reference

--- docs/conf.py
+++ docs/conf.py
@@ -102,8 +102,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
 #
 html_theme = 'furo'
 
-html_favicon = '../assets/logo/OpenMS_transparent_background.png'
-html_logo = '../assets/logo/OpenMS_transparent_background.png'
+html_static_path = ['../_static']
+html_favicon = '../assets/logo/favicon.png'
 html_theme_options = {
     "navigation_with_keys": True,
     "light_css_variables": {
@@ -111,6 +111,8 @@ html_theme_options = {
         "font-size--small": "90%",
         "toc-font-size": "87.5%"
     },
+    "light_logo": "FinalLogo_Nov2024_Versions-01.svg",
+    "dark_logo": "FinalLogo_Nov2024_Versions-02.svg",
 }
 pygments_style = 'sas'
 
@@ -124,7 +126,6 @@ hoverxref_role_types = {'term':'tooltip'}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../_static']
 
 html_css_files = [
     'css/custom.css',
